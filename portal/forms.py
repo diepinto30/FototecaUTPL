16b05837d43fcf10b95a650b84297ed0474ea8ab
diepinto30 <diepinto30@gmail.com>
2019-02-11 23:57:00 -0500

    comentario y editar celulas

--- portal/forms.py
+++ portal/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from portal.models import Actividades
+from portal.models import Actividades, Imagen
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserCreationForm
 
@@ -14,19 +14,49 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
+
 class LoginFrom(forms.Form):
 	username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 	password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
-class CommitForm(forms.ModelForm):
-    iduser = forms.CharField(max_length=30, required=False)
-    idimagen = forms.CharField(max_length=30, required=False)
-    comentario = forms.CharField(max_length=30, required=False)
+class ColeccionesForm(forms.ModelForm):
+    class Meta:
+        model = Imagen
+
+        fields = [
+            'iduser',
+            'nombre',
+            'descripcion',
+            'hayrostros',
+            'direccion',
+            'pais',
+            'ciudad',
+            'latitud',
+            'longitud',
+            'dimenciones',
+            'Tecnica',
+        ]
+        labels = {
+            'iduser': 'User',
+            'nombre': 'Nombre de la imagen',
+            'descripcion': 'Descripcion',
+            'hayrostros': 'Existe algun rostro',
+            'direccion': 'Direccion',
+            'pais': 'Pais',
+            'ciudad':'Ciudad',
+            'latitud':'Latitud',
+            'longitud':'Longitud',
+            'dimenciones':'Dmenciones',
+            'Tecnica':'Tecnica',
+        }
+
+
+class CommitsForm(forms.ModelForm):
     class Meta:
         model = Actividades
         fields = ['iduser', 'idimagen', 'comentario']
-        labels = {'iduser':'user', 'idimagen':'img', 'comentario':'comentario',}
+        labels = {'iduser': 'user', 'idimagen': 'img', 'comentario': 'comentario',}
         widgets = {
             'iduser': forms.TextInput(attrs={'class':'form-control'}),
             'idimagen': forms.TextInput(attrs={'class':'form-control'}),
